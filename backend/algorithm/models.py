4ee2dd26de56f8a10b08d841d37801dbf564b96c
Azen0n <jack.2013.ghost@gmail.com>
2022-12-19 23:17:26 +0500

    Add Models for Algorithm

--- backend/algorithm/models.py
+++ backend/algorithm/models.py
@@ -1,3 +1,121 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from courses.models import Topic, Problem, Difficulty
+
+
+class AbstractProgress(models.Model):
+    points = models.FloatField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def max(self):
+        return self.topic.module.course.topic_max_points
+
+    def is_low_reached(self) -> bool:
+        topic_max_points = self.topic.module.course.topic_max_points
+        threshold_low = self.topic.module.course.topic_threshold_low
+        low = topic_max_points * (threshold_low / self.max)
+        return self.points >= low
+
+    def is_medium_reached(self) -> bool:
+        topic_max_points = self.topic.module.course.topic_max_points
+        threshold_medium = self.topic.module.course.topic_threshold_medium
+        medium = topic_max_points * (threshold_medium / self.max)
+        return self.points >= medium
+
+    def is_high_reached(self) -> bool:
+        topic_max_points = self.topic.module.course.topic_max_points
+        threshold_high = self.topic.module.course.topic_threshold_high
+        high = topic_max_points * (threshold_high / self.max)
+        return self.points >= high
+
+    def is_completed(self) -> bool:
+        return self.points >= self.max
+
+
+class TheoryProgress(AbstractProgress):
+
+    @property
+    def max(self):
+        return self.topic.module.course.topic_theory_max_points
+
+
+class PracticeProgress(AbstractProgress):
+
+    @property
+    def max(self):
+        return self.topic.module.course.topic_practice_max_points
+
+
+class WeakestLinkProblem(models.Model):
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    progress = models.ForeignKey('Progress', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class WeakestLinkTopic(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    progress = models.ForeignKey('Progress', on_delete=models.CASCADE)
+
+
+class Progress(models.Model):
+    class WeakestLinkState(models.TextChoices):
+        FALSE = 'False', _('Weakest Link in process')
+        TRUE = 'True', _('Weakest Link done')
+        NONE = 'None', _('Weakest Link not started')
+
+    user_data = models.ForeignKey('UserData', on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    theory = models.ForeignKey(TheoryProgress, on_delete=models.CASCADE)
+    practice = models.ForeignKey(PracticeProgress, on_delete=models.CASCADE)
+    weakest_link_state = models.CharField(max_length=5,
+                                          choices=WeakestLinkState.choices,
+                                          default=WeakestLinkState.NONE)
+
+    @property
+    def points(self) -> float:
+        return self.theory.points + self.practice.points
+
+    @property
+    def practice_difficulty(self) -> dict[Difficulty, int]:
+        normal = self.topic.module.course.difficulty_threshold_normal
+        hard = self.topic.module.course.difficulty_threshold_hard
+        is_medium_reached = self.theory.is_medium_reached()
+        is_high_reached = self.theory.is_high_reached()
+
+        return {
+            Difficulty.EASY: normal if is_medium_reached else 0,
+            Difficulty.NORMAL: hard if is_high_reached else 0,
+            Difficulty.HARD: 0
+        }
+
+    @property
+    def max_difficulty(self) -> Difficulty:
+        normal = self.topic.module.course.difficulty_threshold_normal
+        hard = self.topic.module.course.difficulty_threshold_hard
+
+        if self.theory.is_high_reached():
+            return Difficulty.HARD
+        if self.practice_difficulty[Difficulty.NORMAL] >= hard:
+            return Difficulty.HARD
+        if self.theory.is_medium_reached():
+            return Difficulty.NORMAL
+        if self.practice_difficulty[Difficulty.EASY] >= normal:
+            return Difficulty.NORMAL
+        return Difficulty.EASY
+
+
+class UserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class UserAnswer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    is_solved = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- backend/courses/models.py
+++ backend/courses/models.py
@@ -13,6 +13,25 @@ class Course(models.Model):
     duration = models.IntegerField()
     thumbnail = models.FileField(upload_to='thumbnails/', blank=True, null=True)
 
+    topic_threshold_low = models.IntegerField(default=61)
+    topic_threshold_medium = models.IntegerField(default=76)
+    topic_threshold_high = models.IntegerField(default=91)
+    topic_theory_max_points = models.IntegerField(default=40)
+    topic_practice_max_points = models.IntegerField(default=60)
+
+    points_easy = models.IntegerField(default=2)
+    points_normal = models.IntegerField(default=5)
+    points_hard = models.IntegerField(default=9)
+
+    difficulty_threshold_normal = models.IntegerField(default=3)
+    difficulty_threshold_hard = models.IntegerField(default=2)
+
+    sub_topic_points_coefficient = models.FloatField(default=1 / 3)
+
+    @property
+    def topic_max_points(self) -> float:
+        return self.topic_theory_max_points + self.topic_practice_max_points
+
     def __str__(self):
         return self.title
 
@@ -41,7 +60,7 @@ class Module(models.Model):
 
     @property
     def problem_count(self):
-        return Problem.objects.filter(topic__module=self.pk).count()
+        return Problem.objects.filter(main_topic__module=self.pk).count()
 
     @property
     def total_time(self):
@@ -75,6 +94,13 @@ class Attachment(models.Model):
         return self.name
 
 
+class Difficulty(models.IntegerChoices):
+    """Problem Difficulty."""
+    EASY = 1, _('Легкое')
+    NORMAL = 2, _('Нормальное')
+    HARD = 3, _('Сложное')
+
+
 class Problem(models.Model):
     """Topic Problem model."""
 
@@ -84,19 +110,15 @@ class Problem(models.Model):
         MULTIPLE_CHOICE_CHECKBOX = 'Multiple Choice Checkbox', _('Выбор нескольких вариантов')
         FILL_IN_SINGLE_BLANK = 'Fill In Single Blank', _('Заполнение пропуска')
 
-    class Difficulty(models.IntegerChoices):
-        """Problem Difficulty."""
-        EASY = 1, _('Легкое')
-        NORMAL = 2, _('Нормальное')
-        HARD = 3, _('Сложное')
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.TextField()
     description = models.TextField()
     type = models.CharField(max_length=100, choices=Type.choices)
     difficulty = models.IntegerField(choices=Difficulty.choices)
-    value = models.IntegerField()
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    main_topic = models.ForeignKey(Topic, on_delete=models.CASCADE,
+                                   related_name='problem_main_set')
+    sub_topics = models.ManyToManyField(Topic, related_name='problem_sub_set',
+                                        blank=True)
 
     def __str__(self):
         return self.title
