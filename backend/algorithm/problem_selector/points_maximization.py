2c08b9913a1a59da574aa95b9acb17abe16fb55e
Azenon <jack.2013.ghost@gmail.com>
2023-04-05 19:50:58 +0500

    Add points threshold comparison to prevent users from earning points solely by completing easy problems

--- backend/algorithm/problem_selector/points_maximization.py
+++ backend/algorithm/problem_selector/points_maximization.py
@@ -1,7 +1,9 @@
+import math
 from django.contrib.auth.models import User
 from django.db.models import QuerySet
 
 from algorithm.models import Progress
+from answers.points_management import DIFFICULTY_TO_POINTS_THRESHOLD
 from config.settings import Constants
 from courses.models import Semester, Problem, Difficulty, Type, THEORY_TYPES
 
@@ -30,7 +32,7 @@ def calculate_problem_value(user: User, semester: Semester, problem: Problem) ->
     skill_level_coefficient = Constants.AVERAGE_SKILL_LEVEL / progress.skill_level
     weighted_time_to_solve = problem.time_to_solve_in_seconds * skill_level_coefficient
     points = calculate_points_if_problem_solved_correctly(progress, problem)
-    return weighted_time_to_solve / points
+    return math.inf if points == 0 else weighted_time_to_solve / points
 
 
 def calculate_points_if_problem_solved_correctly(progress: Progress, problem: Problem) -> float:
@@ -44,18 +46,45 @@ def calculate_points_if_problem_solved_correctly(progress: Progress, problem: Pr
     )
     main_topic_points = POINTS_BY_DIFFICULTY[problem.difficulty]
     sub_topic_points = POINTS_BY_DIFFICULTY[problem.difficulty] * Constants.SUB_TOPIC_POINTS_COEFFICIENT
-    total_points = points_if_problem_solved_correctly(progress, main_topic_points, Type(problem.type))
+    total_points = main_topic_points_if_problem_solved_correctly(progress, main_topic_points, problem)
     for sub_topic_progress in sub_topics_progresses:
-        total_points += points_if_problem_solved_correctly(sub_topic_progress,
-                                                           sub_topic_points, Type(problem.type))
+        total_points += sub_topic_points_if_problem_solved_correctly(sub_topic_progress,
+                                                                     sub_topic_points, problem)
     return total_points
 
 
-def points_if_problem_solved_correctly(progress: Progress, points: float, problem_type: Type) -> float:
-    """Возвращает количество баллов, которое получит пользователь по теме,
+def main_topic_points_if_problem_solved_correctly(progress: Progress,
+                                                  points: float,
+                                                  problem: Problem) -> float:
+    """Возвращает количество баллов, которое получит пользователь по основной теме,
     если решит теоретическое или практическое задание правильно.
     """
-    current_points, max_points = get_points_by_problem_type(progress, problem_type)
+    points_threshold = DIFFICULTY_TO_POINTS_THRESHOLD[problem.difficulty]
+    if progress.points >= points_threshold:
+        return 0
+    elif progress.points + points >= points_threshold:
+        points = points_threshold - progress.points
+    return points_if_problem_solved_correctly(progress, points, problem)
+
+
+def sub_topic_points_if_problem_solved_correctly(progress: Progress,
+                                                 points: float,
+                                                 problem: Problem) -> float:
+    """Возвращает количество баллов, которое получит пользователь по подтеме,
+    если решит теоретическое или практическое задание правильно.
+    """
+    if progress.points >= Constants.TOPIC_THRESHOLD_MEDIUM:
+        return 0
+    elif progress.points + points >= Constants.TOPIC_THRESHOLD_MEDIUM:
+        points = Constants.TOPIC_THRESHOLD_MEDIUM - progress.points
+    return points_if_problem_solved_correctly(progress, points, problem)
+
+
+def points_if_problem_solved_correctly(progress: Progress, points: float, problem: Problem) -> float:
+    """Возвращает количество баллов, которое получит пользователь,
+    если решит теоретическое или практическое задание правильно.
+    """
+    current_points, max_points = get_points_by_problem_type(progress, Type(problem.type))
     if current_points >= max_points:
         return 0
     elif current_points + points > max_points:
