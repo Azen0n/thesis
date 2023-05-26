66a4fe29d1757b8825fc5b38ed9d8969835b4ee5
Azenon <jack.2013.ghost@gmail.com>
2023-05-26 14:44:25 +0500

    Change project settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -169,7 +169,11 @@ STATICFILES_DIRS = [
     'static/'
 ]
 
+STATIC_ROOT = '/var/www/als/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
